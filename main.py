4549f4a6450f7fe21b04d7b73c047d1db419af72
danielfx1985 <111473888+danielfx1985@users.noreply.github.com>
2023-03-31 00:00:22 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 def drow():
     led.plot(x, y)
-    basic.pause(200)
+    basic.pause(100)
+    music.play_tone(262, music.beat(BeatFraction.EIGHTH))
     led.unplot(x, y)
+
+def on_button_pressed_a():
+    start()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    control.reset()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def start():
+    global y, x
+    for index in range(randint(0, 50)):
+        if x == 0:
+            drow()
+            y += 1
+        if y == 4:
+            drow()
+            x += 1
+        if x == 4:
+            drow()
+            y += -1
+        if y == 0:
+            drow()
+            x += -1
+    led.plot(x, y)
 y = 0
 x = 0
 x = 0
 y = 0
-for index in range(100):
-    if x == 0:
-        y += 1
-        drow()
-    if y == 4:
-        x += 1
-        drow()
-    if x == 4:
-        y += -1
-        drow()
-    if y == 0:
-        x += -1
-        drow()
+
+def on_forever():
+    pass
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 function drow () {
     led.plot(x, y)
     basic.pause(100)
-    music.playTone(262, music.beat(BeatFraction.Eighth))
     led.unplot(x, y)
 }
 input.onButtonPressed(Button.A, function () {
@@ -11,7 +10,7 @@ input.onButtonPressed(Button.B, function () {
     control.reset()
 })
 function start () {
-    for (let index = 0; index < randint(0, 50); index++) {
+    for (let index = 0; index < randint(16, 150); index++) {
         if (x == 0) {
             drow()
             y += 1
@@ -35,6 +34,30 @@ let y = 0
 let x = 0
 x = 0
 y = 0
+let ax = [
+0,
+1,
+2,
+3,
+4
+]
+let ay = [0, 4]
+let by = [
+0,
+1,
+2,
+3,
+4
+]
+let bx = [0, 4]
+let cx = ax._pickRandom()
+let cy = ay._pickRandom()
+let dx = bx._pickRandom()
+let dy = by._pickRandom()
 basic.forever(function () {
 	
+})
+basic.forever(function () {
+    led.plot(cx, cy)
+    led.plot(dx, dy)
 })
